58a31c9e163ba6a11333a9bc8668d89a711e8a43
Eric Echemane <eechemane29@gmail.com>
2022-09-23 18:50:39 +0800

    fixed cors

--- index.ts
+++ index.ts
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 4000;
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({ origin: "*" }));
 app.use(express.json({ limit: '50mb' }));
 
 // routes
