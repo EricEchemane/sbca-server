37ff23ae0bec4f13fa0a2fb36adff1ebb0c8c8c0
Eric Echemane <eechemane29@gmail.com>
2022-12-22 21:11:41 +0800

    get all guests route

--- routes/guest.route.ts
+++ routes/guest.route.ts
@@ -1,21 +1,37 @@
-import getDbConnection from "@db/db-connection";
-import { RequestError } from "@utils/request";
-import normalize from "@utils/response-normalizer";
-import { Router, Request, Response } from "express";
+import getDbConnection from '@db/db-connection';
+import { RequestError } from '@utils/request';
+import normalize from '@utils/response-normalizer';
+import { Router, Request, Response } from 'express';
 
 const guestRoute = Router();
 
-guestRoute.post("/", normalize(async (req: Request, _: Response) => {
-    const { fullName, purposeOfVisit } = req.body;
-    if (!fullName || !purposeOfVisit) throw new RequestError(400, "Please provide a name and purpose of visit");
+guestRoute.post(
+	'/',
+	normalize(async (req: Request, _: Response) => {
+		const { fullName, purposeOfVisit } = req.body;
+		if (!fullName || !purposeOfVisit)
+			throw new RequestError(400, 'Please provide a name and purpose of visit');
 
-    const db = await getDbConnection();
-    const { Guest } = db.models;
+		const db = await getDbConnection();
+		const { Guest } = db.models;
 
-    const guest = new Guest({ fullName, purposeOfVisit });
-    await guest.save();
+		const guest = new Guest({ fullName, purposeOfVisit });
+		await guest.save();
 
-    return guest;
-}));
+		return guest;
+	})
+);
+
+guestRoute.get(
+	'/',
+	normalize(async (req: Request, _: Response) => {
+		const db = await getDbConnection();
+		const { Guest } = db.models;
+
+		const guests = await Guest.find();
+
+		return guests;
+	})
+);
 
 export default guestRoute;
